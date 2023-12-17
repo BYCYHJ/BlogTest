26135566770b44334da918eb8fab13d8c693935e
baichenhao <2630296915@qq.com>
2023-12-17 13:33:31 +0800

    create baseentity

---
+++ Identity.Domain/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Identity.Domain
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; init; }//实体Id
+        public BaseEntity() {
+            Id = Guid.NewGuid();
+        }
+    }
+}
