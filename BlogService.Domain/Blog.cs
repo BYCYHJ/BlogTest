7531bbf88c30575703b3fa9bb612718176836739
晨昊 白 <2630296915@qq.com>
2023-12-26 00:50:54 +0800

    Update BlogService Entity

--- BlogService.Domain/Blog.cs
+++ BlogService.Domain/Blog.cs
@@ -8,5 +8,12 @@ namespace BlogService.Domain
         public string Content { get; private set; }//文章内容
         public List<object>? Tag {  get; private set; }//文章标签
         public string UserId {  get; init; }//作者
+        public int StartCount {  get; private set; }//点赞数
+
+        public Blog(string title,string content,string userId) {
+            Title = title;
+            Content = content;
+            UserId = userId;
+        }
     }
 }
---
+++ BlogService.Domain/Comment.cs
@@ -0,0 +1,33 @@
+﻿using BlogDomainCommons;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogService.Domain
+{
+    public class Comment  : BaseEntity
+    {
+        public string Content {  get; init; }//评论内容
+        public string BlogId { get; init; }//所属博客id
+        public string UserId {  get; init; }//发表评论的用户Id
+        public int StartCount {  get; private set; }//点赞数
+        public bool IsReplyComment {  get; init; }//是否为回复评论的评论,0=n,1=y
+        public string? ReplyCommentId {  get; init; }//所属的评论的id
+
+        public Comment(string content,string blogId,string userId,bool isReplyComment,string replyCommentId = null) {
+            Content = content;
+            BlogId = blogId;
+            UserId = userId;
+            IsReplyComment = isReplyComment;
+            if(replyCommentId is not null)
+            {
+                ReplyCommentId = replyCommentId;
+            }
+        }
+
+    }
+}
